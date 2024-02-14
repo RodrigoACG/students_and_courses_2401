cf3f632262b6475935564b93576e93273d1734c0
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-14 10:55:56 -0700

    Iteration 1 done

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -11,9 +11,18 @@ RSpec.describe Student do
     expect(@student_1).to be_instance_of(Student)
   end
   
-  it 'has a name'do
+  it 'has info'do
     expect(@student_1.name).to eq("Morgan")
+    expect(@student_1.age).to eq(21)
+    expect(@student_1.scores).to eq([])
+    expect(@student_1.log_score(89)).to eq(89)
+    expect(@student_1.log_score(78)).to eq(78)
+    expect(@student_1.scores).to eq([89, 78])
+    expect(@student_1.grade).to eq(83.5)
+
   end 
+
+
 
 
   
