a156388c11502d6bfb835b35437da9276f65c06a
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-14 09:40:22 -0700

    Name test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,10 +2,19 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  it "exists" do
-    morgan = Student.new({name: "Morgan", age: 21})
-    expect(morgan).to be_instance_of(Student)
+  before(:each) do 
+    @student_1 = Student.new({name: "Morgan", age: 21})
+    
   end
+  it "exists" do
+    @student_1 = Student.new({name: "Morgan", age: 21})
+    expect(@student_1).to be_instance_of(Student)
+  end
+  
+  it 'has a name'do
+    expect(@student_1.name).to eq("Morgan")
+  end 
+
 
   
 end
