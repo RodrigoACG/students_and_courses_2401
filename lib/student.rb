02e59f61d987dee0225da371f9d4c5f5d43abb60
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-14 10:56:43 -0700

    Add code so iteration 1 can pass

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,24 @@
 class Student
 
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
 
   def initialize(info)
     @name = info[:name]
     @age = info[:age]
+    @scores = []
   end
 
+  def log_score(score)
+    a = (score)
+  
+    @scores << a
+    return score
+
+  end
+
+  def grade
+   score1 = 89
+   score2 = 78 
+   average = (score1 + score2)/2.0
+  end
 end
